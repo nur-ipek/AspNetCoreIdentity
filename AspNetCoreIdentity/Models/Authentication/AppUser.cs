aec77d7a2905047c8d6443c5b33ea9eddc5ec9e3
lcadmin <lcadmin@DESKTOP-HR2SJKL>
2023-11-16 18:06:26 +0300

    Add project files.

---
+++ AspNetCoreIdentity/Models/Authentication/AppUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace AspNetCoreIdentity.Models.Authentication
+{
+    public class AppUser: IdentityUser
+    {
+    }
+}
---
+++ AspNetCoreIdentity/Models/Context/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using AspNetCoreIdentity.Models.Authentication;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreIdentity.Models.Context
+{
+    public class AppDbContext: IdentityDbContext<AppUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> dbContext) : base(dbContext) { }
+    }
+
+}
+
